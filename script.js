5e2cd5a7b76b8754da21507c62ccdf77710eff2b
Alexandre Patta <alexandrelpf@hotmail.com>
2024-10-09 15:49:30 -0300

    bug fixes and api implamentation

--- script.js
+++ script.js
@@ -1,83 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var button1 = document.getElementById("expButton1")
-  var button2 = document.getElementById("expButton2")
-  var button3 = document.getElementById("expButton3")
-
-  button1.addEventListener("click", function () {
-    changeClass(button1, [button2, button3])
-    changeText(button1)
-    changeContent(button1)
-  })
-
-  button2.addEventListener("click", function () {
-    changeClass(button2, [button1, button3])
-    changeText(button2)
-    changeContent(button2)
-  })
-
-  button3.addEventListener("click", function () {
-    changeClass(button3, [button1, button2])
-    changeText(button3)
-    changeContent(button3)
-  })
-
-  function changeClass(targetButton, otherButtons) {
-    targetButton.classList.remove("border-gray-400", "opacity-75")
-    targetButton.classList.add("border-green-500", "opacity-100")
-    otherButtons.forEach((button) => {
-      button.classList.remove("border-green-500", "opacity-100")
-      button.classList.add("border-gray-400", "opacity-75")
-    })
-  }
-
-  function changeText(targetButton) {
-    var title = document.getElementById("title")
-    if (targetButton.id === "expButton1") {
-      title.textContent = "Analysis and Test Execution"
-    } else if (targetButton.id === "expButton2") {
-      title.textContent = "International Quality Assurance"
-    } else {
-      title.textContent = "Defect Management and Improvements"
-    }
-  }
-
-  function changeContent(targetButton) {
-    var cont1 = document.getElementById("cont1")
-    var cont2 = document.getElementById("cont2")
-    var cont3 = document.getElementById("cont3")
-    var cont4 = document.getElementById("cont4")
-
-    if (targetButton.id === "expButton1") {
-      cont1.textContent =
-        "Carrying out systems analysis of mobile applications "
-      cont2.textContent = "Execution of exploratory tests "
-      cont3.textContent = "Conducting functional tests "
-      cont4.textContent = "Executing performance tests "
-    } else if (targetButton.id === "expButton2") {
-      cont1.textContent =
-        "Commitment to meeting international quality standards "
-      cont2.textContent = "Adoption of globally recognized best practices "
-      cont3.textContent =
-        "Comprehensive testing for global usability and compliance "
-      cont4.textContent =
-        "Continuous refinement to surpass international quality norms and enhance user satisfaction "
-    } else {
-      cont1.textContent = "Detailed record of identified faults"
-
-      cont2.textContent = "Proactive suggestion for improvements"
-      cont3.textContent =
-        "Use of the Jira Atlassian tool for efficient management of defects and improvements"
-      cont4.textContent =
-        "Implementation of best practices to prevent future defects"
-    }
-  }
-})
-
 var loadingBars = [
   { id: "loading-bar1", numberId: "loading-number1", finalWidth: 65 },
   { id: "loading-bar2", numberId: "loading-number2", finalWidth: 70 },
   { id: "loading-bar3", numberId: "loading-number3", finalWidth: 75 },
   { id: "loading-bar4", numberId: "loading-number4", finalWidth: 80 },
+  { id: "loading-bar5", numberId: "loading-number5", finalWidth: 50 },
+  { id: "loading-bar6", numberId: "loading-number6", finalWidth: 60 },
+  { id: "loading-bar7", numberId: "loading-number7", finalWidth: 70 },
+  { id: "loading-bar8", numberId: "loading-number8", finalWidth: 80 },
+  { id: "loading-bar9", numberId: "loading-number9", finalWidth: 80 },
 ]
 
 function handleIntersection(entries, observer) {
@@ -129,3 +59,167 @@ function increaseLoading(barId, numberId, finalWidth) {
 
   animateLoading()
 }
+
+document
+  .getElementById("professional-btn")
+  .addEventListener("click", function () {
+    document.getElementById("professional-section").scrollIntoView({
+      behavior: "smooth", // Rolagem suave
+    })
+  })
+document
+  .getElementById("experience-btn")
+  .addEventListener("click", function () {
+    document.getElementById("experience-section").scrollIntoView({
+      behavior: "smooth", // Rolagem suave
+    })
+  })
+document.getElementById("contact-btn").addEventListener("click", function () {
+  document.getElementById("contact-section").scrollIntoView({
+    behavior: "smooth", // Rolagem suave
+  })
+})
+
+//API TESTE//???????????????????????????????????????????????????????????????????????????????????????
+
+let accessToken = ""
+
+document.getElementById("searchButton").addEventListener("click", getArtistInfo)
+
+function getToken() {
+  const data = new URLSearchParams({
+    grant_type: "client_credentials",
+    client_id: "8943114079a24d9585a6ce716ac54f26",
+    client_secret: "0dd611f4cb8f4df981eabd64015bd598",
+  })
+
+  return fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: data.toString(),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Erro na requisição: " + response.status)
+      }
+      return response.json()
+    })
+    .then((data) => {
+      accessToken = data.access_token
+      return accessToken
+    })
+}
+
+function getArtistId(artistName) {
+  return getToken().then(() => {
+    return fetch(
+      `https://api.spotify.com/v1/search?q=${encodeURIComponent(
+        artistName
+      )}&type=artist`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Erro na requisição: " + response.status)
+        }
+        return response.json()
+      })
+      .then((data) => {
+        const artists = data.artists.items
+        if (artists.length > 0) {
+          return artists[0].id
+        } else {
+          throw new Error("Nenhum artista encontrado.")
+        }
+      })
+  })
+}
+
+function getArtistInfo() {
+  const artistName = document.getElementById("search").value
+  getArtistId(artistName)
+    .then((artistId) => {
+      fetch(`https://api.spotify.com/v1/artists/${artistId}`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Erro na requisição: " + response.status)
+          }
+          return response.json()
+        })
+        .then((data) => {
+          document.getElementById("artistName").textContent = data.name
+          document.getElementById("popularityNumber").textContent =
+            data.popularity
+
+          document.getElementById("artistImage").src = data.images[0]?.url || ""
+        })
+        .catch((error) => {
+          console.error("Erro:", error)
+        })
+    })
+    .catch((error) => {
+      console.error("Erro ao obter o ID do artista:", error)
+    })
+  getArtistTopTrack()
+}
+
+function getArtistTopTrack() {
+  const artistName = document.getElementById("search").value
+
+  getArtistId(artistName)
+    .then((artistId) => {
+      return fetch(
+        `https://api.spotify.com/v1/artists/${artistId}/top-tracks?market=US`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      )
+    })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Erro na requisição: " + response.status)
+      }
+      return response.json()
+    })
+    .then((data) => {
+      document.getElementById("topTrack01").textContent = data.tracks[0].name
+      document.getElementById("albumTrack01").src =
+        data.tracks[0].album.images[0]?.url || ""
+
+      document.getElementById("topTrack02").textContent = data.tracks[1].name
+      document.getElementById("albumTrack02").src =
+        data.tracks[1].album.images[0]?.url || ""
+
+      document.getElementById("topTrack03").textContent = data.tracks[2].name
+      document.getElementById("albumTrack03").src =
+        data.tracks[2].album.images[0]?.url || ""
+
+      document.getElementById("topTrack04").textContent = data.tracks[3].name
+      document.getElementById("albumTrack04").src =
+        data.tracks[3].album.images[0]?.url || ""
+
+      document.getElementById("topTrack05").textContent = data.tracks[4].name
+      document.getElementById("albumTrack05").src =
+        data.tracks[4].album.images[0]?.url || ""
+    })
+    .catch((error) => {
+      console.error("Erro:", error)
+    })
+
+}
+
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/**npx tailwindcss -i ./assets/style.css -o ./assets/tailwind.css --watch */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
